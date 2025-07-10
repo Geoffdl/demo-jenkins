57c95bfa05c5cd343995e80a5901ddfbf6029441
Geoffdl <geoffroydelorme@gmail.com>
2025-07-10 17:01:47 +0200

    file update

--- src/main/java/fr/diginamic/Main.java
+++ src/main/java/fr/diginamic/Main.java
@@ -8,7 +8,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        String str = "LEEEERRRROOOOYYYY JEENNNNNNNNNNNNNKINSSS";
+        String str = "Leroy Jenkins";
         try
         {
             String art = FigletFont.convertOneLine(str);
